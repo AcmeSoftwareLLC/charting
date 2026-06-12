0bc327401110304dc37291041e2c1453cc864ee7
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2026-06-12 21:52:22 +0545

    fix: update analysis_options.yaml to exclude generated files and improve type safety in messages_en.dart

--- packages/acme_chart/lib/generated/intl/messages_en.dart
+++ packages/acme_chart/lib/generated/intl/messages_en.dart
@@ -20,7 +20,7 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static String m0(text) => "No results for \"${text}\"";
+  static String m0(String text) => "No results for \"${text}\"";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
